9dbcc6a2f89ade773aa0ce020163c7c3318c8e5e
vantosik <vaclavantosik@hotmail.com>
2014-08-20 19:59:47 +0200

    Moving rectangle

--- Script.js
+++ Script.js
@@ -1,11 +1,27 @@
 
+var position = 0;
 
 function main()
 {
     var canvasElement = document.getElementById('canvas');
     var canvas = canvasElement.getContext('2d');
 
-    canvas.strokeRect(100, 100, 120, 120);
+    var timerId = window.setInterval(function () { tick(canvas) }, 1000);
+}
+
+function tick(canvas)
+{
+    canvas.clearRect(0, 0, 500, 500);
+
+    position = position + 5;
+    canvas.fillText(position, 10, 10, 500);
+
+    canvas.save();
+    
+    canvas.translate(position, 0);
+    canvas.strokeRect(100, 100, 150, 150);
+
+    canvas.restore();
 }
 
 addEventListener("load", main);
