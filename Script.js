4327be55db754d1eced5826472346902abf4b414
vaclav-antosik <vaclavantosik@hotmail.com>
2014-08-28 16:47:18 +0200

    A lot of boxes --> bad way ... I will need writable bitmap

--- Script.js
+++ Script.js
@@ -25,18 +25,31 @@ function tick(canvas)
 	}
 	canvas.fillStyle = 'red';
 
-	for(var i = 0; i < 5; i++)
-	{
-		canvas.save();
-        canvas.translate(100 + (200 * i), 200 + position);
-        canvas.rotate(rotation * Math.PI);
+	for (var q = 0; q < 3000; q = q + 10)
+    {
+        for (var w = 0; w < 1000; w = w + 10)
+        {
+            canvas.save();
+            canvas.translate(position, 0);
+            canvas.strokeRect(q, w, 10, 10);
+            canvas.fillRect(q-2, w-2, 8, 8);
 
-        canvas.strokeRect(-50, -50, 100, 100);
-        canvas.fillRect(-48, -48, 96, 96);
-        canvas.clearRect(-46, -46, 90, 90);
-		
-		canvas.restore();
-	}
+            canvas.restore();
+        }
+    }
+
+
+	    for (var i = 0; i < 5; i++) {
+	        canvas.save();
+	        canvas.translate(100 + (200 * i), 200 + position);
+	        canvas.rotate(rotation * Math.PI);
+
+	        canvas.strokeRect(-50, -50, 100, 100);
+	        canvas.fillRect(-48, -48, 96, 96);
+	        canvas.clearRect(-46, -46, 90, 90);
+
+	        canvas.restore();
+	    }
  }
 
 function getRandomColor() {
